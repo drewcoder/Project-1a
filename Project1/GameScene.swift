519f9cf2ee43e5d4cafb9069ab479f9287482431
drewcoder <drewcoder@gmail.com>
2022-02-26 11:41:25 -0500

    Added the player and sets its position. Also added comments on what the different functions do in the program. Deleted the Hello World node.

--- Project1/GameScene.swift
+++ Project1/GameScene.swift
@@ -10,17 +10,33 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // Player is created here so it can be used throughout the game
+    
+    let player = SKSpriteNode(imageNamed: "player-rocket.png")
+    
+    
     override func didMove(to view: SKView) {
-        // this method is called when your game scene is ready to run
+        
+        // This method is called when your game scene is ready to run
+        // It loads background picture and sets its position at the back
+        
         let background = SKSpriteNode(imageNamed: "space.jpg")
         background.zPosition = -1
         addChild(background)
+        
+        // We modify the snow particles to create space dust and move time ahead to fill screen at game start
         
         if let particles = SKEmitterNode(fileNamed: "SpaceDust") {
             particles.advanceSimulationTime(10)
             particles.position.x = 512
             addChild(particles)
         }
+        
+        // This displays the player and sets its position to the left side of the screen
+        
+        player.position.x = -400
+        player.zPosition = 1
+        addChild(player)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
